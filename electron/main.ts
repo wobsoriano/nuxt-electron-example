a1fc6c7ce7a95da183fa67dc0ed2eb31e574d579
wobsoriano <sorianorobertc@gmail.com>
2022-12-03 00:31:40 -0800

    Add message

--- electron/main.ts
+++ electron/main.ts
@@ -8,6 +8,7 @@ app.whenReady().then(() => {
   if (process.env.VITE_DEV_SERVER_URL) {
     win.loadURL(process.env.VITE_DEV_SERVER_URL)
   } else {
-    win.loadFile(path.join(__dirname, '../dist/index.html'))
+    // win.loadFile(path.join(__dirname, '../dist/index.html'))
+    // Figure out how to run built app
   }
 })
